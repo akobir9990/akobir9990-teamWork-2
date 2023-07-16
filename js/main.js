f8c7e8f62449e7e10a6ccc149189f9b5642cc076
feryzb <feruzbscit@gmail.com>
2023-07-17 02:10:04 +0500

    feruz

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 window.addEventListener("scroll", function () {
-  shrink();
+  toggleBacktop();
 });
 
-let navbar = document.getElementById("navbar");
+let backtop = document.getElementById("backtop");
 
-function shrink() {
-  if (scrollY > 100) {
-    navbar.classList.add("navbar-shrink");
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
   } else {
-    navbar.classList.remove("navbar-shrink");
+    backtop.classList.remove('backtop-show')
   }
 }
+
+
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
